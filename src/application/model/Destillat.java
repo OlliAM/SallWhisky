212106a9f9e82b606699817cf47b3e44d313da9a
OrimasutaRS <Olli97+1@live.dk>
2025-11-25 16:55:13 +0100

    Fad og destillatklasser

--- src/application/model/Destillat.java
+++ src/application/model/Destillat.java
@@ -1,19 +1,55 @@
 package application.model;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.time.LocalDate;
+import java.util.*;
 
 public abstract class Destillat implements Drinkable {
     private double mængdeL;
     private String kommentar;
     private double alkoholprocent;
 
+    //Linkattributter
+    private Map<LocalDate, ArrayList<Fad>> fadHistorik;
 
 //    Vi laver setter-metode for de variable, som kan tilføjes senere, således at et destillat kan oprettes i systemet
 //    Før det er færdigt
 
 
+    public Destillat() {
+
+        this.fadHistorik = new HashMap<>();
+    }
+
+    public void addFad(Fad fad, LocalDate dato) {
+        if(dato == null) {
+            dato = LocalDate.now();
+        }
+        ArrayList<Fad> fade = new ArrayList<>();
+        if(fadHistorik.containsKey(dato)) {
+            fade = fadHistorik.get(dato);
+        }
+
+        if(!fade.contains(fad)) {
+            fade.add(fad);
+            fadHistorik.put(dato, fade);
+            fad.addIndhold(this, dato);
+        }
+
+    }
+
+    public void removeFad(Fad fad, LocalDate dato) {
+        ArrayList<Fad> fade = fadHistorik.get(dato);
+        if(fade != null) {
+            if(fade.contains(fad)) {
+                fade.remove(fad);
+                fad.removeIndhold(this, dato);
+            }
+        }
+    }
+
+    public Map<LocalDate, ArrayList<Fad>> getFadHistorik() {
+        return fadHistorik;
+    }
 
     public void setMængdeL(double mængdeL) {
         this.mængdeL = mængdeL;
--- src/application/model/Fad.java
+++ src/application/model/Fad.java
@@ -1,8 +1,110 @@
 package application.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Fad implements Storable{
     //Fadene kan typisk bruges mange gange.
     //Størrelserne på fadene kan variere og Sall ser på anvendelse af mindre fade på 30 og 50 liter, men også på
     //store, som kan indeholde 100, 190 eller 250 liter væske
 
+    private int fadNr;
+    private Fadtype fadtype;
+    private double kapacitetL;
+    private String oprindelse;
+    private double mængdeL;
+
+    //Linkattributter
+    private Map<LocalDate, ArrayList<Drinkable>> indholdshistorik;
+    private Destillat indhold;
+
+    public Fad(int fadNr, Fadtype fadtype, double kapacitetL, String oprindelse, double mængdeL) {
+        this.fadNr = fadNr;
+        this.fadtype = fadtype;
+        this.kapacitetL = kapacitetL;
+        this.oprindelse = oprindelse;
+        this.mængdeL = mængdeL;
+        indholdshistorik = new TreeMap<>();
+    }
+
+    public void addIndhold(Drinkable drinkable, LocalDate dato) {
+        if(dato == null) {
+            dato = LocalDate.now();
+        }
+        ArrayList<Drinkable> indhold = new ArrayList<>();
+        if(indholdshistorik.containsKey(dato)) {
+            indhold = indholdshistorik.get(dato);
+        }
+
+        if(!indhold.contains(drinkable)) {
+            indhold.add(drinkable);
+            indholdshistorik.put(dato, indhold);
+
+            if(drinkable instanceof Destillat) {
+                ((Destillat) drinkable).addFad(this, dato);
+            }
+        }
+    }
+
+    public void removeIndhold(Drinkable drinkable, LocalDate dato) {
+        ArrayList<Drinkable> indhold = indholdshistorik.get(dato);
+        if(indhold != null) {
+            if(indhold.contains(drinkable)) {
+                indhold.remove(drinkable);
+                if(drinkable instanceof Destillat) {
+                    ((Destillat) drinkable).removeFad(this, dato);
+                }
+            }
+        }
+    }
+
+    public Map<LocalDate, ArrayList<Drinkable>> getIndholdshistorik() {
+        return indholdshistorik;
+    }
+
+    public Destillat getIndhold() {
+        return indhold;
+    }
+
+    public int getFadNr() {
+        return fadNr;
+    }
+
+    public void setFadNr(int fadNr) {
+        this.fadNr = fadNr;
+    }
+
+    public Fadtype getFadtype() {
+        return fadtype;
+    }
+
+    public void setFadtype(Fadtype fadtype) {
+        this.fadtype = fadtype;
+    }
+
+    public double getKapacitetL() {
+        return kapacitetL;
+    }
+
+    public void setKapacitetL(double kapacitetL) {
+        this.kapacitetL = kapacitetL;
+    }
+
+    public String getOprindelse() {
+        return oprindelse;
+    }
+
+    public void setOprindelse(String oprindelse) {
+        this.oprindelse = oprindelse;
+    }
+
+    public double getMængdeL() {
+        return mængdeL;
+    }
+
+    public void setMængdeL(double mængdeL) {
+        this.mængdeL = mængdeL;
+    }
 }
