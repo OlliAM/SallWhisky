33dbba3c660f0b27418d21cb4008ccb20bfdb56f
OrimasutaRS <Olli97+1@live.dk>
2025-11-25 14:36:23 +0100

    Fad og destillatklasser

--- src/application/model/KombiDestillat.java
+++ src/application/model/KombiDestillat.java
@@ -10,5 +10,22 @@ public class KombiDestillat extends Destillat{
         this.destillater = new ArrayList<>();
     }
 
+    public void add(Destillat destillat) {
+        if(!destillater.contains(destillat)) {
+            destillater.add(destillat);
+        }
+    }
 
+    public void remove(Destillat destillat) {
+        destillater.remove(destillat);
+    }
+
+    public Destillat getChild(int i) {
+        if(destillater.size() >= i) {
+            return destillater.get(i);
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException("Destillatet består kun af 2");
+        }
+    }
 }
