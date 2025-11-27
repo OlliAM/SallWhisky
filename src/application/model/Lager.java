a411c3d6e0978224a674300f09954decd6e156f1
Simon Stensgaard <STcena1@hotmail.com>
2025-11-27 21:30:53 +0100

    Finished JavaDoc for Lager-class & methods

--- src/application/model/Lager.java
+++ src/application/model/Lager.java
@@ -77,7 +77,13 @@ public class Lager {
         return this.antalFlasker;
     }
 
-    // Create & add new Reol-instance to internal Lager-class.
+    /**
+     * <p1> Opretter et instanse af {@code Reol-klassen} med prekonfigureret lagerplads
+     * og lagrer den i tilhørende {@code Lager-klasse}.</p1><br>
+     * @param ID String id
+     * @param pladser nummer af tilgængelige lagerpladser
+     * @return Reol
+     */
     public Reol opretReol(String ID, int pladser) {
         // Instantiate the new Reol-class to store.
         Storable[] nyePladser = new Storable[pladser];
@@ -92,7 +98,17 @@ public class Lager {
         return newReol;
     }
 
-    // Store the specified product at index.
+    /**
+     * <p1>Metode til at lagre valgte instans af {@code Storable} i specificerede {@code Reol}.
+     * Metoden opfanger hvilken subklasse objektet tilhører og optæller herefter
+     * interne {@code antalFade} eller {@code antalFlasker} variabler.</p1>
+     * @param reol reol til at gemme produktet i
+     * @param pladsNr tilhørende nummer af lagerplads
+     * @param produkt valgte produkt til at lagre
+     * @throws IndexOutOfBoundsException Hvis {@code pladsNr} param er uden for Reolens kapacitet
+     * @throws RuntimeException Hvis den valgte lagerplads allerede anvendes.
+     * @return void
+     */
     public void gemPåReol(Reol reol, int pladsNr, Storable produkt) {
         // Call the storage method in Reol-instace -> Handles Errors.
         try {
@@ -109,7 +125,16 @@ public class Lager {
         }
     }
 
-    // Store the specified product at index.
+    /**
+     * <p1>Metode til at udtage et {@code Storable} objekt fra den specificerede lagerplads.
+     * Metoden opfanger hvilken subklasse objektet tilhører og nedsætter herefter
+     * interne {@code antalFade} eller {@code antalFlasker} variabler. </p1>
+     * @param reol reol til at tage produktet fra
+     * @param pladsNr tilhørende nummer af lagerplads
+     * @throws IndexOutOfBoundsException Hvis {@code pladsNr} param er uden for Reolens kapacitet
+     * @throws RuntimeException Hvis den valgte lagerplads er uden {@code Storable} objekt.
+     * @return Storable
+     */
     public Storable tagFraReol(Reol reol, int pladsNr) {
         // Initiate new Storable variable.
         Storable produkt = null;
@@ -133,8 +158,9 @@ public class Lager {
     }
 
     /**
-     *
-     * @return
+     * <p1>Metode til at udarbejde en liste af individuelle lagerpladser der på nuværende tidspunkt
+     * ikke indeholder et {@code Storable} objekt</p1>
+     * @return {@code Arraylist<String>}
      */
     public ArrayList<String> getTommePladser() {
 
@@ -151,8 +177,13 @@ public class Lager {
         return  list;
     }
 
-    // Linear Search method (Override).
-    // FadNr-search.
+    /**
+     * <p1><b><i>**Overloaded**</i></b></p1><br>
+     * <p1>Metode til at søge efter et individuelt {@code Fad} objekt på det tilhørende lager
+     * ud fra objektets ID nummer. Udfører en Linear Søgning eftersom placeringen er sporadisk</p1>
+     * @param fadNr ID nummer for fadet som søges
+     * @return String
+     */
     public String søgPåLager(int fadNr) {
 
         // Iterate over entire list of Reol-instances.
@@ -168,8 +199,14 @@ public class Lager {
         return "Cask with ID: " + fadNr + ", not found!";
     }
 
-    // Linear Search method (Override).
-    // Fadtype-search.
+    /**
+     * <p1><b><i>**Overloaded**</i></b></p1><br>
+     * <p1>Metode til at søge efter alle {@code Fad} objekter på det tilhørende lager
+     * som besidder den specifikke {@code fadType} variabel.
+     * Udfører en Linear Søgning eftersom placeringen er sporadisk</p1>
+     * @param fadtype Typen af {@code Fad} der søges
+     * @return {@code Arraylist<String>} / {@code null}
+     */
     public ArrayList<String> søgPåLager(String fadtype) {
 
         // Initiate new arraylist instance.
@@ -192,8 +229,14 @@ public class Lager {
         }
     }
 
-    // Linear Search method (Override).
-    // Destillat-search.
+    /**
+     * <p1><b><i>**Overloaded**</i></b></p1><br>
+     * <p1>Metode til at søge efter alle {@code Fad} objekter på det tilhørende lager
+     * som indeholder det specifikke {@code destillat}.
+     * Udfører en Linear Søgning eftersom placeringen er sporadisk</p1>
+     * @param destillat Typen af {@code destillat} som søges
+     * @return {@code Arraylist<String>} / {@code null}
+     */
     public ArrayList<String> søgPåLager(Destillat destillat) {
 
         // Initiate new arraylist instance.
