1078802e25efde08a975bb0121060cfb58571f19
OrimasutaRS <Olli97+1@live.dk>
2025-12-08 16:35:05 +0100

    Storable tests

--- test/application/model/StorableTest.java
+++ test/application/model/StorableTest.java
@@ -22,20 +22,50 @@ class StorableTest {
         //Act & assert
         assertNull(plads.getVare());
         fad.gemPåPlads(plads);
+
         assertEquals(fad, plads.getVare());
+        assertEquals(plads, fad.getPlads());
     }
 
     @Test
     void gemPåPlads_optagetPlads() {
-        //
+        //Arrange
+        Fad fad2 = new Fad(2, "Egetræ", 50, "Italien");
+        fad.gemPåPlads(plads);
+
+        //Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> fad2.gemPåPlads(plads));
+
+        //Assert
+        assertNull(fad2.getPlads());
+        assertEquals(fad, plads.getVare());
+        assertEquals("Den valgte plads er allerede optaget", exception.getMessage());
     }
 
     @Test
     void gemPåPlads_storableAlleredePåLager() {
+        //Arrange
+        Plads plads2 = plads.getReol().getPladser()[1];
+        fad.gemPåPlads(plads);
 
+        //Act
+        fad.gemPåPlads(plads2);
+
+        //Assert
+        assertEquals(fad, plads2.getVare());
+        assertNull(plads.getVare());
     }
 
     @Test
     void fjernFraPlads() {
+        //Arrange
+        fad.gemPåPlads(plads);
+
+        //Act
+        fad.fjernFraPlads();
+
+        //Assert
+        assertNull(plads.getVare());
+        assertNull(fad.getPlads());
     }
 }
